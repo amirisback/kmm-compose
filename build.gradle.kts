4b277c71d7ba5837d27ae4237f2c42cf8bdb65bd
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-10-09 16:24:14 +0700

    update: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     java
     kotlin("jvm") version "1.5.21"
     id("org.jetbrains.compose") version "1.0.0-alpha3"
-    id("maven-publish")
+    `maven-publish`
 }
 
 /** Artifact groupId. */
@@ -26,7 +26,7 @@ val myArtifactVersion: String = project.version.toString()
 
 /** My GitHub username. */
 val myGithubUsername = "amirisback"
-val myGithubDescription = "Repository Sample For Native Kotlin Library"
+val myGithubDescription = "Jetpack Compose KMM UI Experimental"
 val myGithubHttpUrl = "https://github.com/${myGithubUsername}/${myArtifactId}"
 val myGithubIssueTrackerUrl = "https://github.com/${myGithubUsername}/${myArtifactId}/issues"
 val myLicense = "Apache-2.0"
@@ -74,19 +74,19 @@ val sourcesJar by tasks.creating(Jar::class) {
     }
 }
 
-// More info on `publishing`:
-//   https://docs.gradle.org/current/userguide/publishing_maven.html#publishing_maven:resolved_dependencies
-// More info on authenticating with personal access token (myDeveloperId and myArtifactName must be lowercase):
-//   https://docs.github.com/en/packages/guides/configuring-gradle-for-use-with-github-packages#authenticating-to-github-packages
 publishing {
 
     publications {
-        register("gprRelease", MavenPublication::class) {
-            groupId = myArtifactGroup
-            artifactId = myArtifactId
-            version = myArtifactVersion
+
+        register("release", MavenPublication::class) {
 
             from(components["java"])
+
+            groupId = myArtifactGroup
+
+            artifactId = myArtifactId
+
+            version = myArtifactVersion
 
             artifact(sourcesJar)
 
@@ -94,43 +94,13 @@ publishing {
                 packaging = "jar"
                 name.set(myArtifactId)
                 description.set(myGithubDescription)
-                url.set(myGithubHttpUrl)
-                scm {
-                    url.set(myGithubHttpUrl)
-                }
-                issueManagement {
-                    url.set(myGithubIssueTrackerUrl)
-                }
-                licenses {
-                    license {
-                        name.set(myLicense)
-                        url.set(myLicenseUrl)
-                    }
-                }
-                developers {
-                    developer {
-                        id.set(myGithubUsername)
-                        name.set(myDeveloperName)
-                    }
-                }
             }
 
         }
     }
 
     repositories {
-
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/${myGithubUsername}/${myArtifactId}")
-            credentials {
-                username = System.getenv("GITHUB_PACKAGES_USERID")
-                password = System.getenv("GITHUB_PACKAGES_PUBLISH_TOKEN")
-            }
-        }
-
         maven { url = uri("https://jitpack.io") }
-
     }
 
 }
